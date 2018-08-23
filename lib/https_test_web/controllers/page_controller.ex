5fd7f229b9c45063c47bce3a2a4d75c432d81414
Paweł Obrok <pawel.obrok@gmail.com>
2018-08-23 17:17:19 +0200

    Add button

--- lib/https_test_web/controllers/page_controller.ex
+++ lib/https_test_web/controllers/page_controller.ex
@@ -4,4 +4,10 @@ defmodule HttpsTestWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def create(conn, _params) do
+    conn
+    |> put_flash(:info, "Success!")
+    |> render("index.html")
+  end
 end
--- lib/https_test_web/router.ex
+++ lib/https_test_web/router.ex
@@ -2,21 +2,23 @@ defmodule HttpsTestWeb.Router do
   use HttpsTestWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", HttpsTestWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+    post("/", PageController, :create)
   end
 
   # Other scopes may use custom stacks.
